bba11d19f295528ba2d79e71fc2c01d11732793d
pv297 <p.v.2977187@gmail.com>
2023-05-11 14:35:36 +0300

    config and launch

---
+++ src/main/java/screens/SplashScreen.java
@@ -0,0 +1,20 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SplashScreen extends BaseScreen{
+    public SplashScreen(AppiumDriver<AndroidElement> driver) {
+        super(driver);
+    }
+    @FindBy(xpath = "//*[@resource-id='com.telran.ilcarro:id/versionText']")
+    AndroidElement versionTextView;
+
+    @FindBy(xpath = "//*[@resource-id='com.telran.ilcarro:id/imageView3']")
+    AndroidElement titleTextView;
+
+    public String getCurrentVersion(){
+        return versionTextView.getText();
+    }
+}
